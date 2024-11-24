b258073d790f7ddf08516a30f0345bd723b413b8
evilicebergs <voiced.fair-0a@icloud.com>
2024-11-24 20:08:07 +0200

    Initial Commit

---
+++ FirebaseMap/FirebaseMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseMapApp.swift
+//  FirebaseMap
+//
+//  Created by Artem Golovchenko on 2024-11-24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
